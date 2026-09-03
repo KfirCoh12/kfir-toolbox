85e903f63c3755d677f96fafea705898c5c50672
KfirCoh12 <98kfir@gmail.com>
2026-09-03 12:17:01 +0300

    Use wide HMI workbench preview

--- electrical-engineering/design-checker/pages/2_Workspace_Preview.py
+++ electrical-engineering/design-checker/pages/2_Workspace_Preview.py
@@ -1,7 +1,7 @@
 """Experimental Board Planner UI/UX sandbox."""
 import streamlit as st
 
-from src.hmi_planner_workspace import render_workspace
+from src.hmi_planner_workspace_v2 import render_workspace
 from src.working_board_baseline import ensure_office_working_baseline
 
 st.set_page_config(
